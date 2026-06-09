de24c929935a399e56cbeed0f9c4cf6022a48bd1
Thanos Kounenis <thanos.kounenis@gmail.com>
2026-06-09 09:38:41 +0100

    ignore node_modules and .git in hister indexing

--- users.d/thanos/hister.nix
+++ users.d/thanos/hister.nix
@@ -7,7 +7,7 @@
     settings.app.search_url = "https://searxng.internal:3443/search?q={query}";
     settings.server.base_url = "https://hister.internal:3443";
     settings.indexer.directories = [
-      { path = "/Users/thanos/Work/projects"; filetypes = [ "md" "txt" "org" ]; }
+      { path = "/Users/thanos/Work/projects"; filetypes = [ "md" "txt" "org" ]; excludes = [ ".git" "node_modules" ]; }
     ];
   };
 
